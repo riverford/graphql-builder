64e3a71fd9a9cef08d303e1ce4933cc096384683
Daniel Neal <danielneal@riverford.co.uk>
2018-04-25 09:46:37 +0100

    Update project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject riverford/graphql-builder "0.1.5-1"
+(defproject riverford/graphql-builder "0.1.5-2"
   :description "A fork of graphql-builder to work with reagent 0.8 and react native"
-  :url "https://github.com/retro/graphql-builder"
+  :url "https://github.com/riverford/graphql-builder"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]
